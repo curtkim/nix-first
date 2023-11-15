16c36c598ebb6edd29ef7157c33a97395e8adf09
curtkim <iamteri@gmail.com>
2023-11-15 20:36:00 +0900

    minor

--- 10-flake/flake.nix
+++ 10-flake/flake.nix
@@ -4,18 +4,18 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-
-    outpus = {self, nixpkgs, flake-utils}: 
-      flake-utils.lib.eachDefaultSystem (system: let
-        pkgs = nixpkgs.legacyPackges.${system};
-      in {
-        devShells.default = pkgs.mkShell {
-	  packages = [pkgs.nodejs];
-	  shellHook = ''
-	    echo "node `${pkgs.nodes}/bin/node --version`"
-	  '';
-	};
-       });
   };
+
+  outputs = {self, nixpkgs, flake-utils}: 
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackges.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+	packages = [pkgs.nodejs];
+	shellHook = ''
+	  echo "node `${pkgs.nodes}/bin/node --version`"
+	'';
+      };
+    });
 }
 
